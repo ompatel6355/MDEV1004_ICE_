3ace8e8354d89aa9118b440c4c06602858c68a35
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-05-16 13:56:27 -0400

    converted project to TypeScript

---
+++ ICE2/Client/Scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    function Start() {
+        console.log("App started...");
+    }
+    window.addEventListener("load", Start);
+})();
+//# sourceMappingURL=app.js.map
