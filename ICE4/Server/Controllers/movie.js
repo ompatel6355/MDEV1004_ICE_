c0427bb4df17491eb13277814a8dc097dcdb3e7d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-05-30 13:38:08 -0400

    added UpdateMovie controller

--- ICE4/Server/Controllers/movie.js
+++ ICE4/Server/Controllers/movie.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AddMovie = exports.DisplayMovieById = exports.DisplayMovieList = void 0;
+exports.UpdateMovie = exports.AddMovie = exports.DisplayMovieById = exports.DisplayMovieList = void 0;
 const movie_1 = __importDefault(require("../Models/movie"));
 const Util_1 = require("../Util");
 function DisplayMovieList(req, res, next) {
@@ -65,4 +65,39 @@ function AddMovie(req, res, next) {
     });
 }
 exports.AddMovie = AddMovie;
+function UpdateMovie(req, res, next) {
+    let id = req.params.id;
+    if (id.length != 24) {
+        res.status(400).json({ success: false, msg: "A valid ID is required to update a movie", data: "" });
+    }
+    else {
+        let genres = (req.body.genres) ? (0, Util_1.SanitizeArray)(req.body.genres) : (0, Util_1.SanitizeArray)("");
+        let directors = (req.body.directors) ? (0, Util_1.SanitizeArray)(req.body.directors) : (0, Util_1.SanitizeArray)("");
+        let actors = (req.body.actors) ? (0, Util_1.SanitizeArray)(req.body.actors) : (0, Util_1.SanitizeArray)("");
+        let writers = (req.body.writers) ? (0, Util_1.SanitizeArray)(req.body.writers) : (0, Util_1.SanitizeArray)("");
+        let movieToUpdate = new movie_1.default({
+            _id: id,
+            movieID: req.body.movieID,
+            title: req.body.title,
+            studio: req.body.studio,
+            genres: genres,
+            directors: directors,
+            writers: writers,
+            actors: actors,
+            length: req.body.length,
+            year: req.body.year,
+            shortDescription: req.body.shortDescription,
+            mpaRating: req.body.mpaRating,
+            criticsRating: req.body.criticsRating
+        });
+        movie_1.default.updateOne({ _id: id }, movieToUpdate)
+            .then(() => {
+            res.status(200).json({ success: true, msg: "Movie updated", data: movieToUpdate });
+        })
+            .catch((err) => {
+            console.error(err);
+        });
+    }
+}
+exports.UpdateMovie = UpdateMovie;
 //# sourceMappingURL=movie.js.map
--- ICE4/Server/Controllers/movie.ts
+++ ICE4/Server/Controllers/movie.ts
@@ -63,6 +63,14 @@ export function DisplayMovieById(req: Request, res: Response, next: NextFunction
     }
 }
 
+/**
+ * This function adds a movie to the database
+ *
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
 export function AddMovie(req: Request, res:Response, next: NextFunction): void
 {
     let genres = (req.body.genres) ?  SanitizeArray(req.body.genres as string) : SanitizeArray("");
@@ -94,4 +102,57 @@ export function AddMovie(req: Request, res:Response, next: NextFunction): void
      {
         console.error(err);
      })
+}
+
+/**
+ * This function updates a movie in the database
+ *
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+export function UpdateMovie(req:Request, res:Response, next:NextFunction): void
+{
+    // endpoint should be /api/update/:id
+    let id = req.params.id;
+
+    // ensure that the id is valid
+    if (id.length != 24)
+    {
+        res.status(400).json({success: false, msg: "A valid ID is required to update a movie", data: ""});
+    }
+    else
+    {
+        let genres = (req.body.genres) ?  SanitizeArray(req.body.genres as string) : SanitizeArray("");
+        let directors = (req.body.directors) ? SanitizeArray(req.body.directors as string) : SanitizeArray("");
+        let actors = (req.body.actors) ? SanitizeArray(req.body.actors as string) : SanitizeArray("");
+        let writers = (req.body.writers) ? SanitizeArray(req.body.writers as string) : SanitizeArray("");
+
+        let movieToUpdate = new Movie({
+            _id: id,
+            movieID: req.body.movieID,
+            title: req.body.title,
+            studio: req.body.studio,
+            genres: genres,
+            directors: directors,
+            writers: writers,
+            actors: actors,
+            length: req.body.length,
+            year: req.body.year,
+            shortDescription: req.body.shortDescription,
+            mpaRating: req.body.mpaRating,
+            criticsRating: req.body.criticsRating
+        });
+
+        Movie.updateOne({_id: id}, movieToUpdate)
+        .then(() =>
+        {
+            res.status(200).json({success: true, msg: "Movie updated", data: movieToUpdate});
+        })
+        .catch((err) =>
+        {
+            console.error(err);
+        })
+    }
 }
