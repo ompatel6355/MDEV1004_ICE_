b43dce8a1c1969df9d9e8f2262984da871408a87
ompatel6355 <74576844+ompatel6355@users.noreply.github.com>
2024-06-09 13:54:31 -0400

    added project files

--- ICE2/Client/Scripts/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(function () {
-    function Start() {
-        console.log("App started...");
-    }
-    window.addEventListener("load", Start);
-})();
-//# sourceMappingURL=app.js.map
--- ICE2/app.js
+++ ICE2/app.js
@@ -1,25 +1,26 @@
-import createError, { HttpError } from 'http-errors';
-import express, { NextFunction, Request, Response } from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-import indexRouter from '../Routes/index';
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// create an express application
-const app = express();
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../Views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../Client')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -27,8 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err: HttpError, req:Request, res:Response, next:NextFunction) 
-{
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +38,4 @@ app.use(function(err: HttpError, req:Request, res:Response, next:NextFunction)
   res.render('error');
 });
 
-export default app;
+module.exports = app;
