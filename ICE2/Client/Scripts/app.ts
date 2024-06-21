4f639b962ce4740510b4d7c5e7643576b94065fc
ompatel6355 <74576844+ompatel6355@users.noreply.github.com>
2024-06-21 10:52:20 -0400

    Initial commit

---
+++ ICE2/Client/Scripts/app.ts
@@ -0,0 +1,11 @@
+// IIFE -- Immediately Invoked Function Expression
+(function () {
+
+    function Start(): void
+    {
+        console.log("App started...");
+    }
+    
+    window.addEventListener("load", Start);
+    
+    })();
--- ICE5/Server/Config/db.ts
+++ ICE5/Server/Config/db.ts
@@ -1,6 +1,5 @@
 let remoteURI = (process.env.MONGO_URI) as string;
 let secret = (process.env.APP_SECRET) as string;
-
 export default {
     remoteURI: remoteURI,
     secret: secret
